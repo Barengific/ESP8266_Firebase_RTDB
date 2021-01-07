ea841d394dff74c641a7c61292beeda7e3ae81c2
Barengific <75044077+Barengific@users.noreply.github.com>
2021-01-07 15:47:50 +0000

    Android and ESP
    Android and ESP source code upload

---
+++ Android_firebase_light/app/src/main/java/com/barengific/basicfirebaseeesp/MainActivity.java
@@ -0,0 +1,86 @@
+package com.barengific.basicfirebaseeesp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+    String TAG = "appers";
+    TextView textView;
+    String a;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.txtField);
+        textView.setText("testers");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                a = value;
+                textView.setText(value);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
+
+        Button clickButton = (Button) findViewById(R.id.btn);
+        clickButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                if(a.equals("on")){
+                    myRef.setValue("off");
+
+                }else if(a.equals("off")){
+                    myRef.setValue("on");
+                }
+            }
+        });
+    }
+    public String val(){
+        //reader
+/*        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                a = value;
+                textView.setText(value);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });*/
+        return a;
+    }
+}
---
+++ ESP_firebase_light/ESP_firebase_light.ino
@@ -0,0 +1,62 @@
+#include <ESP8266WiFi.h>
+#include <FirebaseESP8266.h>
+
+#define w_SSID "wifi_name"
+#define w_PASSWORD "wifi_password"
+
+#define FIREBASE_HOST "https://example.firebaseio.com/"
+#define FIREBASE_AUTH "firebase realtime database sectret key"
+#define relay 5
+
+//Define FirebaseESP8266 data object
+FirebaseData fbdo;
+
+FirebaseJson json;
+
+String msg;
+
+void printResult(FirebaseData &data);
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(relay, OUTPUT);
+  
+  WiFi.begin(w_SSID, w_PASSWORD);
+  Serial.print("Connecting to WiFi Router");
+  while (WiFi.status() != WL_CONNECTED)  {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected to WiFi with the IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+}
+
+void loop() {
+  if (Firebase.getString(fbdo, "/message/")) {
+    msg = fbdo.stringData();
+    Serial.println(msg);
+    if (msg.equals("on")) {
+      lightOn();
+      delay(1000);
+    }
+    if (msg.equals("off")) {
+      lightOff();
+      delay(1000);
+    }
+  }
+}
+
+void lightOn() {
+  digitalWrite(relay, HIGH);
+  delay(400);
+}
+
+void lightOff() {
+  digitalWrite(relay, LOW);
+  delay(400);
+}
